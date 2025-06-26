e2fdcb9e64e67ad8d6bfcd2175cef2bb26c4bf5f
jahnavibarma03 <144089718+jahnavibarma03@users.noreply.github.com>
2025-06-26 11:05:58 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://finance-backend-1epq.onrender.com",
 });
 
 // Send token with every request
